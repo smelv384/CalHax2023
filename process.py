05a8d21ef01a748067bbde924992b0acb4877189
shaemus.melvin <shaemus.melvin@linux12-wd.cs.ucalgary.ca>
2023-02-18 16:03:05 -0700

    Finished process.py

--- process.py
+++ process.py
@@ -1,33 +1,84 @@
 import scraper
+from datetime import time
+rooms, times = scraper.scrape_web("https://contacts.ucalgary.ca/info/cpsc/courses")
 
-room, time = scraper.scraper.web()
-daysAndTime = time.split(" ", 1)
+# Authors: Shaemus Melvin, Harry Lee
+# Takes two arrays of rooms and times/dates, 
 
 def getRoom():
     return room
 
-def getDays():
-    d = list()
-    days = daysAndTime[0]
-    for i in range(0, len(days)):
-        if days[i] == 'M':
+def getDays(time):
+    d = []
+    for i in range(0, len(time)):
+        if time[i] == 'M':
             d.append("Monday")
-        elif days[i] == 'T':
+        elif time[i] == 'T':
             d.append("Tuesday")
-        elif days[i] == 'W':
+        elif time[i] == 'W':
             d.append("Wednesday")
-        elif days[i] == 'R':
+        elif time[i] == 'R':
             d.append("Thursday")
-        else:
+        elif time[i] == 'F':
             d.append("Friday")
     return d
 
-def getBeginTime():
-    t = list()
-    timee = daysAndTime[1].split(" - ")
-    return timee[0]
+def getBeginTime(roomTime):
+    timeStr = roomTime.split()
+    return timeStr[1]
 
-def getEndTime():
-    t = list()
-    timee = daysAndTime[1].split(" - ")
-    return timee[1]
+def getEndTime(roomTime):
+    timeStr = roomTime.split()
+    return timeStr[3]
+
+def getBeginTimeInTimeObject(roomTime):
+    timeString = getBeginTime(roomTime)
+    tim = timeString.split(':')
+    return time(hour = int(tim[0]), minute = int(tim[1]))
+
+def getEndTimeInTimeObject(roomTime):
+    timeString = getEndTime(roomTime)
+    tim = timeString.split(':')
+    return time(hour = int(tim[0]), minute = int(tim[1]))
+
+
+class Booking:
+    building = ""
+    floor  = 0
+    roomNumber  = 0
+    days = []
+    startTime =  time()
+    endTime =  time()
+
+    def __init__(self, room, roomTime):
+        if (room != "TBA" and roomTime != "TBA"):
+            self.building = room.split()[0]
+            if (room.split()[1].isdigit()):
+                self.floor = (room.split())[1][0]
+                self.roomNumber = room.split()[1][1:]
+            else:
+                self.floor = -1
+                self.roomNumber = room.split()[1]
+            self.days = getDays(roomTime)
+            self.startTime = getBeginTimeInTimeObject(roomTime)
+            self.endTime = getEndTimeInTimeObject(roomTime)
+        return
+    
+def prettyPrint(booking):
+    print(booking.building)
+    print(booking.floor)
+    print(booking.roomNumber)
+    for day in booking.days:
+        print(day)
+    print(booking.startTime)
+    print(booking.endTime)
+    return
+
+def getBookings():
+    bookings = []
+    for i in range(0, len(rooms)):
+        if (rooms[i] != "TBA" and times[i] != "TBA"):
+            newBooking = Booking(rooms[i], times[i])
+            bookings.append(newBooking)
+
+    return bookings
--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+
 # Author: Shaemus Melvin
 # Takes in the course list url, outputs the list of rooms and times taken according to the course list
 def scrape_web(URL):
@@ -14,13 +16,16 @@ def scrape_web(URL):
     list_items = table.findAll("tr", class_="collapsed primary-row collapsed-primary-row")
     for item in list_items:
         room = item.find("td", style="width:6em")
-        print(room.text)
         rooms.append(room.text)
         time = item.find("td", style="width:11em")
         times.append(time.text)
-        print(time.text)
 
     return rooms, times
+
+def printClass(rooms, times):
+    for i in range(0, len(rooms)):
+        print(rooms[i])
+        print(times[i])
 
 
 rooms, times = scrape_web("https://contacts.ucalgary.ca/info/cpsc/courses")
