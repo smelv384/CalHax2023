45c77d8fd2dd2562ad9044c06211c9179f2d1c00
shaemus.melvin <shaemus.melvin@linux03-wd.cs.ucalgary.ca>
2023-02-18 13:30:59 -0700

    Basic scraping

--- scraper.py
+++ scraper.py
@@ -1,1 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
+
+def scrape_web():
+    URL = "https://contacts.ucalgary.ca/info/cpsc/courses"
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    table = soup.find("div", class_="unitis-course-list")
+    list_items = table.findAll("tr", class_="collapsed primary-row collapsed-primary-row")
+    for item in list_items:
+        
+        print(item.prettify())
+        
+scrape_web()
