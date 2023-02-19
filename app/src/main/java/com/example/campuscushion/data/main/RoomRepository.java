f7f9e8f0c8c7d7ef56c3ac72a657aa9035a1419d
kwalerie <weavermarquez0@gmail.com>
2023-02-18 18:07:05 -0700

    Create RoomRepository with Local Unimplemented Unit Tests

---
+++ app/src/main/java/com/example/campuscushion/data/main/RoomRepository.java
@@ -0,0 +1,4 @@
+package com.example.campuscushion.data.main;
+
+public class RoomRepository {
+}
---
+++ app/src/test/java/com/example/campuscushion/RoomRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.example.campuscushion;
+import org.junit.Test;
+import static org.junit.Assert.*;
+//import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class RoomRepositoryTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void server_connection_canConnect() {
+        fail("Not yet implemented");
+        //assertFalse("Not implemented. Connect to a Database.", true);
+    }
+
+    @Test
+    public void input_isValidated(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void input_validGraphQL(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void server_response_validGraphQL(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void input_canAutoComplete(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void input_button_initiatesQuery(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void input_button_usesDefaults(){
+        fail("Not yet implemented");
+
+    }
+
+    @Test
+    public void input_setting_removeDefaults(){
+        fail("Not yet implemented");
+
+    }
+
+    @Test
+    public void view_response_isVisible(){
+        fail("Not yet implemented");
+
+    }
+
+    @Test
+    public void view_response_PrettyPrint(){
+        fail("Not yet implemented");
+
+    }
+}
