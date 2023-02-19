fcbf8a3624ab48bb8a563f469980d224692f69f1
HeDoLe <harold.d.lee@gmail.com>
2023-02-18 19:04:32 -0700

    Room rate and review implemented

--- process.py
+++ process.py
@@ -1,43 +1,76 @@
-import scraper
+import unicodedata
+from scraper import rooms, times
 from datetime import time
-room, tim = scraper.scraper.web()
-daysAndTime = tim.split(" ", 1)
+
+rooms_edited = list()
+times_edited = list()
+daysAndTime = list()
+
+for i in range(0, len(rooms)):
+    rooms_edited.append(unicodedata.normalize("NFKD", rooms[i]))
+for j in range(0, len(times)):
+    times_edited.append(unicodedata.normalize("NFKD", times[j]))
+    daysAndTime.append(times_edited[j].split(" ", 1))
 
 def getRoom():
-    return room
+    return rooms_edited
 
 def getDays():
     d = list()
-    days = daysAndTime[0]
-    for i in range(0, len(days)):
-        if days[i] == 'M':
-            d.append("Monday")
-        elif days[i] == 'T':
-            d.append("Tuesday")
-        elif days[i] == 'W':
-            d.append("Wednesday")
-        elif days[i] == 'R':
-            d.append("Thursday")
-        else:
-            d.append("Friday")
+    for i in range(0, len(daysAndTime)):
+        days = daysAndTime[i][0]
+        a = []
+        for j in range(0, len(days)):
+            if days[j] == 'M':
+                a.append("Monday")
+            elif days[j] == 'T':
+                a.append("Tuesday")
+            elif days[j] == 'W':
+                a.append("Wednesday")
+            elif days[j] == 'R':
+                a.append("Thursday")
+            else:
+                a.append("Friday")
+        d.append(a)
     return d
 
 def getBeginTime():
     t = list()
-    time = daysAndTime[1].split(" - ")
-    return time[0]
+    for i in range(0, len(daysAndTime)):
+        print(daysAndTime)
+        time = daysAndTime[i][1].split(" - ")
+        t.append(time[0])
+    return t
 
 def getEndTime():
     t = list()
-    time = daysAndTime[1].split(" - ")
-    return time[1]
+    for i in range(0, len(daysAndTime)):
+        time = daysAndTime[i][1].split(" - ")
+        t.append(time[1])
+    return t
 
 def getBeginTimeInTimeObject():
-    timeString = getBeginTime()
-    tim = timeString.split(':')
-    timee = time(hour = tim[0], minute = tim[1])
+    timeList = getBeginTime()
+    ttimeList = list()
+    for i in range(0, len(timeList)):
+        tim = timeList[i].split(':')
+        ttimeList.append(time(hour = tim[0], minute = tim[1]))
+    return time(hour = tim[0], minute = tim[1])
 
 def getEndTimeInTimeObject():
-    timeString = getEndTime()
-    tim = timeString.split(':')
-    timee = time(hour = tim[0], minute = tim[1])
+    timeList = getEndTime()
+    ttimeList = list()
+    for i in range(0, len(timeList)):
+        tim = timeList[i].split(':')
+        ttimeList.append(time(hour = tim[0], minute = tim[1]))
+    return time(hour = tim[0], minute = tim[1])
+print("getDays: ")
+print(getDays())
+print("getBeginTime: ")
+print(getBeginTime())
+print("getEndTime: ")
+print(getEndTime())
+print("getBeginTimeInTimeObject: ")
+print(getBeginTimeInTimeObject())
+print("getEndTimeInTimeObject: ")
+print(getEndTimeInTimeObject())
---
+++ roomReview.py
@@ -0,0 +1,30 @@
+reviews = dict()
+score = dict()
+
+pickedRoom = ""
+
+# allows user to choose a room to rate and review
+def chooseRoomToRateAndReview(room):
+    pickedRoom = room
+
+# rate the picked room out of 5
+def rate():
+    userScore = input("Please rate the quality of your rest in the room out of 5!: ")
+    if userScore > 5 or userScore < 1:
+        print("Please enter a valid number")
+    else:
+        score[pickedRoom] = list()
+        score[pickedRoom].append(userScore)
+
+# write a review for the room
+def writeReview():
+    reviews[pickedRoom] = input("How was the room? Let us know!: ")
+
+# gets the average score of a room
+def getAverageRoomScore(room):
+    averageScore = sum(score[room]) / len(score[room])
+    return averageScore
+
+# gets a list of reviews of a room
+def getRoomReview(room):
+    return reviews[room]
